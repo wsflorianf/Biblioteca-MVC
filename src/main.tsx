bfd0de52927f25d85f1b5781e51559a45f635efe
Santiago Florian <floranwsfrench@gmail.com>
2024-05-26 13:45:06 -0500

    Se retira el modo estricto de React

--- src/main.tsx
+++ src/main.tsx
@@ -85,9 +85,7 @@ const theme = createTheme(
 )
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
     </ThemeProvider>
-  </React.StrictMode>
 )
